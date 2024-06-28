28c35451f1c1345003d77c4aec40fc1bf45de215
priyask2210 <133983142+priyask2210@users.noreply.github.com>
2024-06-28 20:01:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
           </div>
         </div>
         <div className="cardInside">
-          <p>Swarreeyyy,<br></br>Happy birthday</p>
+          <p>Swarreeyyy,<br></br>Piranthanaal nalVaazhthukkal.First of all I miss you so much seriyaaa. Ithu evlo vithyaasama irukum nu therila aana naa kathukitathu vachu unakku ethaachu panni kudukanum nu irunchu. I just wanna say that I love you so much and I would just do almost anything for you. I wish I was there with you right now. I would have taken care of you like my baby.And Please know that you are doing great and I am so so freaking proud of you. Our frinendship is a treasure to me and I am just so grateful to have you in my life. Stay super strong, just like you always do swarr. I am here with you no matter what. Loooovvveee youuuuu. </p>
         </div>
       </div>
     </div>
